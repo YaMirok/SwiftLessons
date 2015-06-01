6efd879d4a94b2bdfe93d2344181550f10b84f70
Damir Sitdikov <YaMirok@MacBook-Pro-Damir.local>
2015-06-02 00:06:45 +0500

    HomeWork1

---
+++ Lesson-01.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let secondName = "Sitdikov"
+let firstName = "Damir"
+let dateOfBirth = "17 november of 1986"
+let age = 28
+let weight = 67.6
+let height = 1.75
+
+println("Full name:\t\t\(firstName) \(secondName)")
+println("Age:\t\t\t\t\(age) years old")
+println("Date of birth:\t\(dateOfBirth)")
+println("Height:\t\t\t\(height) m\nWeight:\t\t\t\(weight) kg")
